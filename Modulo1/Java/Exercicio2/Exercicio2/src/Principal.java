8937391346ee31124edc32350034457f1262e18f
Gustavo Aca <galasdalas50@gmail.com>
2022-04-05 10:53:59 -0300

    Organizando

--- Modulo1/Java/Exercicio2/Exercicio2/src/Principal.java
+++ Modulo1/Java/Exercicio2/Exercicio2/src/Principal.java
@@ -4,32 +4,23 @@ public class Principal {
 
 	public static void main(String[] args) {
 		/*
-		 * Escrever um programa que receba vários números inteiros no teclado. E no
-		 * final imprimir a média dos números múltiplos de 3. Para sair digitar
+		 * /*
+		 * Escrever um programa que receba vï¿½rios nï¿½meros inteiros no teclado. E no
+		 * final imprimir a mï¿½dia dos nï¿½meros mï¿½ltiplos de 3. Para sair digitar
 		 * 0(zero).(DO...WHILE)
 		 */
+		 
 
 		Scanner ler = new Scanner(System.in);
-		int numero = 0, media = 0, soma = 0, cont = 0;
-		boolean verdadeiro = true;
+		int numero = 0, soma = 0;
 
 		do {
-			System.out.print("Insira um numero: ");
+			System.out.println("Insira o numero: ");
 			numero = ler.nextInt();
-
-			if(numero % 3 == 0) {
-				soma+=numero;
-				cont++;
-			}
 			
-
-			if (numero == 0) {
-				verdadeiro = false;
-			}
-		} while (verdadeiro);
+			soma+=numero;
+		} while (numero != 0);
 		
-		media = soma / cont;
-		System.out.printf("A média de todos os numero multiplos de 3 é %d", media);
-
+		System.out.printf("A soma Ã©: %d " + soma);
 	}
 }
