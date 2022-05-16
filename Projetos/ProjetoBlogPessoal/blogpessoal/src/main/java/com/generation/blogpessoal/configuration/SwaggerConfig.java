ee1b1636b3a6a6fc8e2aa9538921b578bce10145
Gustavo dos Anjos <galasdalas50@gmail.com>
2022-05-16 19:44:29 -0300

    Realizado deploy

--- Projetos/ProjetoBlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ Projetos/ProjetoBlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.generation.blogpessoal.configuration;
 
-
-
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,56 +12,74 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
+/**
+ * A Anotação (Annotation) @Configuration indica que a classe é uma fonte
+ * de configuração e definição de Beans.
+ * 
+ */
 @Configuration
 public class SwaggerConfig {
-	
+
+    /**
+     * A Anotação @Bean indica que o método é um bean, ou seja, um objeto que pode ser
+     * injetado em qualquer ponto da sua aplicação.
+     */
 	@Bean
-	public OpenAPI springShopOpenAPI() {
+	public OpenAPI springBlogPessoalOpenAPI() {
 		
-		return new OpenAPI()
+        /** Cria um Objeto da Classe OpenAPI, que gera a documentação no Swagger utilizando 
+         * a especificação OpenAPI.
+         * 
+         */
+        return new OpenAPI()
 				.info(new Info()
-						.title("Projeto blog pessoal")
-						.description("Projeto Loja de Games - Generation Brasil")
-						.version("v0.0.1")
-	            .license(new License()
-	            		.name("generation.org.br")
-	            		.url("http://springdoc.org"))
-	    		.contact(new Contact()
-	    				.name("Gustavo dos Anjos")
-	    			  	.url("https://github.com/GustavoAca")
-	                  	.email("galasdalas50@gmail.com")))
-	    		.externalDocs(new ExternalDocumentation()
-	    				.description("Github")
-	    				.url("https://github.com/GustavoAca/JavaGeneration/tree/main/Projetos/ProjetoBlogPessoal/blogpessoal"));
+					.title("Projeto Blog Pessoal")
+					.description("Projeto Blog Pessoal - Generation Brasil")
+					.version("v0.0.1") // Versão do projeto
+				.license(new License()
+					.name("Generation Brasil")
+					.url("https://brazil.generation.org/")) // Informações  da empresa
+				.contact(new Contact()
+					.name("Gustavo dos Anjos")
+					.url("https://github.com/GustavoAca/JavaGeneration/tree/main/Projetos/ProjetoBlogPessoal/blogpessoal")
+					.email("fernandobroccox1@gmail.com")))
+				.externalDocs(new ExternalDocumentation()
+					.description("Meu LinkedIn")
+					.url("https://www.linkedin.com/in/gustavo-acacio-9876631a1/"));
 	}
-	
+
+    /**
+     * A Classe OpenApiCustomiser permite personalizar o Swagger, baseado na 
+     * Especificação OpenAPI. O Método abaixo, personaliza todas as mensagens 
+     * HTTP Responses (Respostas das requisições) do Swagger.
+     */
 	@Bean
 	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
-		
+
 		return openApi -> {
-			openApi.getPaths().values()
-				.forEach(pathItem -> pathItem.readOperations()
-					.forEach(operation -> {
-							
-					ApiResponses apiResponses = operation.getResponses();
-							
-						apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-						apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
-						apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
-						apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
-						apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
-						apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
-						apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					
-					}
-				)
-			);
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
 		};
 	}
-	
+
+    /**
+     * O Método createApiResponse() adiciona uma descrição (Mensagem), em cada Resposta HTTP.
+     */
 	private ApiResponse createApiResponse(String message) {
-		  
+
 		return new ApiResponse().description(message);
-	
+
 	}
+	
 }
